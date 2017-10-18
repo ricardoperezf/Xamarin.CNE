c16d9840eff7253f4acd3b42f8aaa1568836211a
Ricardo Pérez <ricardojpf98@gmail.com>
2017-10-17 18:17:23 -0600

    Added date.

--- Xamarin.CNE/Xamarin.CNE/MainPage.xaml.cs
+++ Xamarin.CNE/Xamarin.CNE/MainPage.xaml.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Xamarin.CNE
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            TipoDeAccesoZona();
+            fechaDelSistema.Text = DateTime.Now.ToString();
+        }
+
+        private void TipoDeAccesoZona()
+        {
+            AccesoZona.Items.Add("Dificil");
+            AccesoZona.Items.Add("Medio");
+            AccesoZona.Items.Add("Fácil");
+        }
+
+        private void AccesoZona_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var name = AccesoZona.Items[AccesoZona.SelectedIndex];
+            DisplayAlert(name, "Selected value", "OK");
         }
     }
 }
